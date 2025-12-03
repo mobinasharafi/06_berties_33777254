e1bbe21331f9b443d4a0ae87876f2ce66379bebf
Mobina Sharafi <youremail@whatever.com>
2025-12-03 16:59:45 +0000

    Lab 8A added audit and users table.

--- create_db.sql
+++ create_db.sql
@@ -10,7 +10,26 @@ CREATE TABLE IF NOT EXISTS books (
     name   VARCHAR(50),
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
-    -- Creating the  application user
+
+
+ CREATE TABLE IF NOT EXISTS users (
+    id             INT AUTO_INCREMENT,
+    username       VARCHAR(30) UNIQUE,
+    first          VARCHAR(30),
+    last           VARCHAR(30),
+    email          VARCHAR(50) UNIQUE,
+    hashedPassword VARCHAR(255),
+    PRIMARY KEY(id));
+
+ CREATE TABLE IF NOT EXISTS audit_log (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30),
+    success TINYINT(1),
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+ );
+
+
+    -- Creating application user
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop';
 GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
 
